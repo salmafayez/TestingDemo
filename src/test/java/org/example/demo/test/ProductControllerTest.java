33554c2b0991c01e717a89e5a38a3f0c4dc98e7f
salma fayez <salma.fayez.himida@gmail.com>
2022-08-17 14:01:44 +0200

    delete controller

--- src/test/java/org/example/demo/test/ProductControllerTest.java
+++ src/test/java/org/example/demo/test/ProductControllerTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.ResponseEntity;
 import java.util.ArrayList;
@@ -18,14 +17,13 @@ import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @RequiredArgsConstructor
 @AutoConfigureMockMvc
-public class ProductControllerTest2 {
+public class ProductControllerTest {
 
     @SpyBean
     private ProductService productService;
